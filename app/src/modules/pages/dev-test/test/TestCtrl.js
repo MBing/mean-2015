b99bbfb09be4b68b599097022e566aa76c678e9c
Martin Bing <bing.martin.photography@gmail.com>
2017-11-21 08:55:30 +0100

    Add functionality for scope func and var on directive

--- app/src/modules/pages/dev-test/test/TestCtrl.js
+++ app/src/modules/pages/dev-test/test/TestCtrl.js
@@ -1,20 +1,65 @@
 'use strict';
 
-angular.module('myApp').controller('TestCtrl', ['$timeout', 'appHttp', 'UserModel', '$location', function($timeout, appHttp, UserModel, $location) {
+angular.module('myApp').controller('TestCtrl', ['$scope','$timeout', 'appHttp', 'UserModel', '$location', '$q', function($scope, $timeout, appHttp, UserModel, $location, $q) {
 	var vm = this;
 
+	vm.log = [];
+
+	function logIt(text, params) {
+		console.log('logIt:', text);
+		vm.log.push(text);
+	}
 	vm.user =UserModel.load();
 	
 	vm.swipeIt = function(evt, direction, params) {
-		console.log('swipe: '+direction);
+		logIt('swipe: '+direction);
 	};
 	
 	vm.tapIt = function(evt, params) {
-		console.log('tap');
+		logIt('tap');
 	};
 
+	function asyncFunc (var1, callback) {
+		$timeout(function () {
+			logIt('timeout finished');
+			callback();
+		}, 1000);
+		logIt('timeout started');
+	}
+
+	function asyncFuncPromise(var1) {
+		var deferred = $q.defer();
+		$timeout(function () {
+			logIt('timeout finished');
+			deferred.resolve();
+		}, 1000);
+		logIt('timeout started');
+
+		return deferred.promise;
+	}
+	
+	$scope.$on('myEvt', function (evt, params) {
+		logIt('async event done');
+	});
+	
+	vm.triggerAsync = function () {
+		asyncFunc(5, function () {
+			logIt('async done');
+        });
+
+		asyncFuncPromise(5)
+			.then(function () {
+				logIt('async promise resolved');
+			});
+
+		$timeout(function () {
+			$scope.$broadcast('myEvt', {});
+        }, 1000);
+		$scope.$broadcast('myEvt', {});
+    };
+
 	vm.funcOne = function () {
-		console.log('func from controller');
+		logIt('func from controller');
 	};
 
 	vm.scopeOne = 'scope 1';
